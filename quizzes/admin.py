48749d472d6832494f8f9fa715b846555350dfc7
Ali Habibullah <ALLIA1999@HOTMAIL.COM>
2024-05-02 09:15:53 +0300

    created-course-models

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import CustomUser
+from .models import Course, MultipleChoiceQuestion, Choice, FileUploadQuestion
+
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'is_activated', 'is_staff']
@@ -7,6 +9,42 @@ class CustomUserAdmin(admin.ModelAdmin):
 
     def activate_users(self, request, queryset):
         queryset.update(is_activated=True)
+
     activate_users.short_description = "Activate selected users"
 
+
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class MultipleChoiceQuestionInline(admin.StackedInline):
+    model = MultipleChoiceQuestion
+    extra = 1
+    inlines = [ChoiceInline]
+
+
+class FileUploadQuestionInline(admin.StackedInline):
+    model = FileUploadQuestion
+    extra = 1
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    #inlines = [MultipleChoiceQuestionInline, FileUploadQuestionInline]
+
+
+class MultipleChoiceQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'course', 'choice_type')
+    inlines = [ChoiceInline]
+
+class FileUploadQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'course')
+
+# Re-register these if needed
+admin.site.register(MultipleChoiceQuestion, MultipleChoiceQuestionAdmin)
+admin.site.register(FileUploadQuestion, FileUploadQuestionAdmin)
--- quizzes/models.py
+++ quizzes/models.py
@@ -6,3 +6,44 @@ class CustomUser(AbstractUser):
     is_activated = models.BooleanField(default=False)
     identification_number = models.CharField(max_length=100, unique=True)
 
+
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    course = models.ForeignKey(Course, related_name='questions', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.text
+
+
+class MultipleChoiceQuestion(Question):
+    course = models.ForeignKey(Course, related_name='mc_questions', on_delete=models.CASCADE)
+    choice_type = models.CharField(max_length=8, choices=[('single', 'Single Answer'), ('multiple', 'Multiple Answers')], default='single')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(MultipleChoiceQuestion, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class FileUploadQuestion(Question):
+    course = models.ForeignKey(Course, related_name='file_upload_questions', on_delete=models.CASCADE)
